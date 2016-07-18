936bccfc55240361bc56226876516ceeeb4e0268
Ivan <pozharskyi@gmail.com>
2016-07-18 16:30:02 +0300

    Refactor views

--- resources/views/layouts/user.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-@section('in navbar')
-
-    <li><a href="{{ route('books.index') }}">Books</a></li>
-    <li><a href="{{ route('users.index') }}">Users</a></li>
-
-@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.authUser')
 
 @section('content')
 <div class="container">
