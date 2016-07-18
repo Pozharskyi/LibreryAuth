7662d05280d2e98493ebc9f7e3ede3c3a386bb57
Ivan <pozharskyi@gmail.com>
2016-07-18 13:01:31 +0300

    ADD link to facebook sign-in

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -57,6 +57,12 @@
                                 <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
                             </div>
                         </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <a class="btn btn-block btn-social btn-facebook" href="{{ url('/auth/facebook') }}">Sign in with facebook</a>
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
